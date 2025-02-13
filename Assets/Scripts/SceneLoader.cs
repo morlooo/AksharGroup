8fcccc3d9d9342c2f9a530385bd1b68faa60549c
unknown <prajapatimayur444@gmail.com>
2025-02-13 20:20:49 +0530

    add last scene mostly done project

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,7 +6,9 @@ public class SceneLoader : MonoBehaviour
     // This method will be called when the button is clicked
     public void LoadScene(string sceneName)
     {
+        Debug.Log("click");
         // Load the scene by name
         SceneManager.LoadScene(sceneName);
+       // Debug.Log("click");
     }
 }
